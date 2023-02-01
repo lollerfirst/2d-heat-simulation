ca4d9ee1f23d00a504b4f1e2bd62353e09ca7120
Tommaso Bruschi <43107113+lollerfirst@users.noreply.github.com>
2023-02-01 15:27:17 +0100

    Create plot_script.sh

---
+++ plot_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gnuplot -e "
+set terminal pngcairo background rgb '#ffffffff';
+set xrange[0:30];
+set yrange[0:30];
+set cbrange[19:100];
+n_frames = 300;
+do for [i=0:n_frames-1]
+{
+  set output sprintf('images/heat_diffusion%03d.png', i);
+  plot 'heat_diffusion.dat' index i with image;
+}
+"
+convert -verbose -loop 0 -delay 10 images/heat_diffusion* animation.gif
